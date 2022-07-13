09d04f724ea5fcc6bc18d7d7701b3a0204b96c52
Leonardo de Farias <71597308+Tofudog@users.noreply.github.com>
2022-07-13 17:48:27 -0400

    first likely working solution

--- CompleteSearch/LoadBalancing.cpp
+++ CompleteSearch/LoadBalancing.cpp
@@ -2,29 +2,71 @@
 #include <map>
 #include <set>
 using namespace std;
-typedef set<int> si;
+typedef map<int, int> mmi;
+typedef long long ll;
 
+//going through the xbounds
+/*
+for q1, x1=1 and x2=xans; y1=1 and y2=yans
+for q2, x1=xans and x2=xmax; y1=1 and y2=yans
+for q3, x1=1 and x2=xans; y1=yans and y2=ymax
+for q1, x1=xans and x2=xmax; y1=yans and y2=ymax
+*/
+//this only works for quad 1 and 2
+int quadScore(int x1, int x2, int y1, int y2, mmi pos) {
+    int score = 0;
+    for (int i=x1; i<x2; i+=2) {
+        if (pos.find(i)==pos.end()) {continue;}
+        else {
+            for (const auto &j : xtoY[i]) {
+                if (j > y2) {break;}
+                score++;
+            }
+        }
+    }
+}
+
+// took me three hours to make my own solution!
 int main() {
-    freopen("balancing.in", "r", stdin);
-    freopen("balancing.out", "w", stdout);
     int n, b; cin >> n >> b;
-    map<int, si> pos; si left, right, down, up;
-    int xmin, ymin = b; int xmax, ymax = 0;
+    mmi xtoY, ytoX;
+    int xmin = b; int ymin = b; int xmax = 1; int ymax = 1;
     for (int i=0; i<n; i++) {
         int xi, yi; cin >> xi >> yi;
-        xmin = max(xmin, xi); ymin = max(ymin, yi);
+        if (xtoY.find(xi)==xtoY.end()) {
+            xtoY[xi]=0;
+        }
+        if (ytoX.find(yi)==ytoX.end()) {
+            ytoX[yi]=0;
+        }
+        xtoY[xi]++; ytoX[yi]++;
+        xmin = min(xmin, xi); ymin = min(ymin, yi);
         xmax = max(xmax, xi); ymax = max(ymax, yi);
-        if (pos.find(xi)==pos.end()) {pos[xi]={y1}; continue;}
-        pos[xi].insert(yi);
     }
-    //filling left and right, then down and up
-    left={xmin};
-    for (const auto &item : pos) {
-        if (item.first > xmin) {right.insert(item.first);}
+    int left = xtoY[xmin]; int right = n-left;
+    int res = abs(right-left);
+    int xans = xmin+1;
+    for (int x=xmin+1; x<xmax; x+=2) {
+        left += xtoY[x-1]; right -= xtoY[x-1];
+        if (res > abs(right-left)) {
+            res = abs(right - left);
+            xans = x+2;
+        }
     }
-    //for down/up, iterate through both left and right and find < or > curr.
-    int xbound=xmin; int ybound=ymin;
-    for (int i=1; i<xbound; i+=2) {
-        
+    int down = ytoX[ymin]; int up = n-down;
+    res = abs(up-down);
+    int yans = ymin+1;
+    for (int y=ymin+1; y<ymax; y+=2) {
+        down += ytoX[y-1]; up -= ytoX[y-1];
+        if (res > abs(up - down)) {
+            res = abs(up - down);
+            yans = y+2;
+        }
     }
+    int q1 = 0; int q2 = 0; int q3 = 0; int q4 = 0;
+    q1 = quadScore(1, xans, 1, yans, xtoY);
+    //fix for the other quadrants...
+
+
+    cout << min( min(q1, q2), min(q3, q4) );
 }
