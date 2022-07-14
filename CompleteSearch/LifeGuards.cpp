e2b8dbd904d6ec8dd7fb9c1f7a58d21912a4fcf2
Leonardo de Farias <71597308+Tofudog@users.noreply.github.com>
2022-07-14 14:55:31 -0400

    Add files via upload

--- CompleteSearch/LifeGuards.cpp
+++ CompleteSearch/LifeGuards.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
-#include <map>
 using namespace std;
-typedef map<int, int> mmi;
 
-//for true brute force, just find which pts are covered and add.
+//time taken to solve: 25m 32s + ~10m to debug
 int main() {
-    int N; cin >> N;
-    mmi pts;
-    for (int i=0; i<N; i++) {
-        int start, end; cin >> start >> end;
-        pts[start]=end;
+    freopen("lifeguards.in", "r", stdin);
+    freopen("lifeguards.out", "w", stdout);
+    int n; cin >> n;
+    int start[n]; int end[n];
+    int low = 1000; int high = 0;
+    for (int i=0; i<n; i++) {
+        int s, e; cin >> s >> e;
+        low = min(low, s); high = max(high, e);
+        start[i] = s; end[i] = e;
     }
-    int ans=0;
-    //brute forcing through firing each guard
-    for (int fire=0; fire<N; fire++) {
-        int curr=0;
-        mmi intervals;
-        for (int x=0; x<N; x++) {
-            if (x==fire) {continue;}
-            auto iter = intervals.begin();
-            while (iter != intervals.end()) {
-                if ()
-                ++iter;
+    int ans = 0;
+    for (int f=0; f<n; f++) {
+        int curr = 0;
+        bool shift[high+1] = {false};
+        for (int c=0; c<n; c++) {
+            if (f==c) {continue;}
+            //marking true at each pos at least 1 cow appears in
+            for (int x=start[c]; x<end[c]; x++) {
+                shift[x]=true;
             }
         }
+        //if true at loc. in shift, then add one to the score
+        for (bool cow : shift) {
+            if (cow) {curr++;}
+        }
+        ans = max(ans, curr);
     }
+    cout << ans;
 }
